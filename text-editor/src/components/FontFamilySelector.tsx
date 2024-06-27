bfb793b6e13d4177f034bb8ed8160f35f68a0a46
sachinmohite <sachinmohite708@gmail.com>
2024-06-27 13:04:35 +0530

    done

--- text-editor/src/components/FontFamilySelector.tsx
+++ text-editor/src/components/FontFamilySelector.tsx
@@ -1,22 +1,25 @@
-
-
 // import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
 
 // const FontFamilySelector: React.FC<{
 //   onSelectFamily: (fontFamily: string) => void;
 // }> = ({ onSelectFamily }) => {
-//   let [fonts, setFonts] = useState<string[]>([]);
+//   const [fonts, setFonts] = useState<string[]>([]);
+//    console.log(fonts[46]);
 
 //   useEffect(() => {
 //     const fetchFonts = async () => {
 //       const apiKey = 'AIzaSyBcF5Gi8V_LdiPvPSLh9mcvjMzhbrj1Yl8'; // Replace with your Google API key
-//       const response = await axios.get(
-//         `https://www.googleapis.com/webfonts/v1/webfonts?key=${apiKey}&sort=popularity&fields=items(family)`
-//       );
-//       const topFonts = response.data.items.slice(0, 25).map((item: any) => item.family);
-//       setFonts(topFonts);
-    
+//       try {
+//         const response = await axios.get(
+//           `https://www.googleapis.com/webfonts/v1/webfonts?key=${apiKey}&sort=popularity`
+//         );
+//         const allFonts = response.data.items.map((item: any) => item.family);
+//         const uniqueFonts = Array.from(new Set<string>(allFonts)); // Ensure fonts are unique
+//         setFonts(uniqueFonts.slice(0, 50)); // Set all fetched unique fonts
+//       } catch (error) {
+//         console.error('Error fetching fonts:', error);
+//       }
 //     };
 
 //     fetchFonts();
@@ -32,9 +35,12 @@
 //       <label htmlFor="fontFamily">Font Family:</label>
 //       <select id="fontFamily" onChange={handleFamilyChange}>
 //         {fonts.map((font, index) => (
-//           <option key={index} value={font}>
+//           (index==0||index==10||index==46||index==17)
+//            && <option key={index} value={font}>
 //             {font}
 //           </option>
+          
+         
 //         ))}
 //       </select>
 //     </div>
@@ -42,6 +48,8 @@
 // };
 
 // export default FontFamilySelector;
+
+
 
 
 import React, { useState, useEffect } from 'react';
@@ -76,9 +84,10 @@ let FontFamilySelector: React.FC<{
       <label htmlFor="fontFamily">Font Family:</label>
       <select id="fontFamily" onChange={handleFamilyChange}>
         {fonts.map((font, index) => (
-          <option key={index} value={font}>
+      <option key={index} value={font}>
             {font}
           </option>
+          
         ))}
       </select>
     </div>
